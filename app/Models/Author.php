4484961e33c0b63efd52a697e9af3a85bdd80a10
pascalllacroix <webiepscf@gmail.com>
2020-09-29 10:57:17 +0200

    Intégration de la liaison Post belongsTo Author

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Author extends Model
 {
     use HasFactory;
-    public function author() {
-      return $this->belongsTo('App\Models\Author');
-    }
 }
--- resources/views/posts/_index.blade.php
+++ resources/views/posts/_index.blade.php
@@ -1,7 +1,7 @@
 {{--
   ./resources/views/posts/_index.blade.php
   Variables disponibles:
-    $posts ARRAY(OBJ(id, titre, sousTitre, texte, datePublication, user))
+    $posts ARRAY(OBJ(id, titre, sousTitre, texte, datePublication, author_id, author OBJ(id, fristname, lastname, ...)))
 --}}
 <!-- ADD A POST -->
 <div class="clearfix">
@@ -19,7 +19,7 @@
         P{{ $post->sousTitre }}
       </h3>
     </a>
-    <p class="post-meta">Posted on {{ $post->datePublication }}</p>
+    <p class="post-meta">Posted on {{ $post->datePublication }} by {{ $post->author->firstname }} {{ $post->author->lastname }}</p>
   </div>
   <hr>
 @endforeach
